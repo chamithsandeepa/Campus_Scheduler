a56b7d685c4fabf715728643ca371d417e536ae0
chamithsandeepa <chamithsandeepa321@gmail.com>
2026-03-18 22:46:30 +0530

    Refactor: Enhance MongoDB connection handling and logging

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -1,12 +1,16 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-	try {
-		
-		const conn = await mongoose.connect(process.env.MONGO_URI);
-		console.log(`MongoDB Connected`);
-	} catch (error) {
-		console.log("Error connection to MongoDB: ", error.message);
-		process.exit(1); // 1 is failure, 0 status code is success
-	}
+  try {
+    mongoose.connection.on("connected", () => {
+      console.log("MongoDB connected successfully to campus_scheduler");
+    });
+    const baseUri = process.env.MONGO_URI.endsWith('/') 
+      ? process.env.MONGO_URI.slice(0, -1) 
+      : process.env.MONGO_URI;
+    const conn = await mongoose.connect(`${baseUri}/campus_scheduler`);
+  } catch (error) {
+    console.log("Error connection to MongoDB: ", error.message);
+    process.exit(1); // 1 is failure, 0 status code is success
+  }
 };
